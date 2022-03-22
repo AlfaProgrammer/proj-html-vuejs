7bb9624c29d78fb0622a8cddd0a990ae8706434c
AlfaProgrammer <alexloghin@outlook.it>
2022-03-22 14:08:00 +0100

    importato immagini e settato componenti per la creazione del header + alcuni mixins

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+     <div class="container">
+        <div class="top-bar"> HELLOOOOOO </div>
+        <div class="log"></div>
+        <NavLinks>
+            <template v-slot:searchOnSite> 
+               <li> icon search </li>
+            </template>
+        </NavLinks>
+     </div>
+  </div>
+</template>
+
+<script>
+    import NavLinks from './NavLinks.vue'
+
+    export default {
+        name: "AppHeader",
+        props:{
+            linkTitle:{
+                type: String,
+                required: true
+            },
+            linkSrc:{
+                type: String,
+                required:true
+            }
+        },
+        components:{
+            NavLinks
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+   .header{
+        div.container{        
+        
+            div.top-bar{
+                height: 30px;
+                background-color: salmon;
+            }
+    }
+   }
+</style>
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="links-wrapper">
+      <ul>
+        <li v-for="(el, i) in navLinks" :key="i">
+            <a href="el.linkSrc"> {{ el.linkTitle }} </a>
+        </li>
+        <slot name="searchOnSite"></slot>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name:'NavLinks',
+    data(){
+        return{
+            navLinks:[
+                {
+                    linkTitle:'Home',
+                    linkSrc:'#'
+                },
+                {
+                    linkTitle:'Recepies',
+                    linkSrc:'#'
+                },
+                {
+                    linkTitle:'Places',
+                    linkSrc:'#'
+                },
+                {
+                    linkTitle:'Blog',
+                    linkSrc:'#'
+                },
+                {
+                    linkTitle:'About',
+                    linkSrc:'#'
+                },
+                {
+                    linkTitle:'Contacts',
+                    linkSrc:'#'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+    @import '@/assets/scss/mixins.scss';
+    @import '@/assets/scss/_shared.scss';
+
+    
+   nav.links-wrapper{
+       display: flex;
+       column-gap: 40px;
+       row-gap: 20px;
+       align-items: center;
+       justify-content: center;
+       border: 2px solid red;
+
+        ul{
+            display: contents;
+
+            li{
+                color: $text;
+                background-color: goldenrod;
+                
+                a{
+                    @include anchorDefault;
+                }
+                &:hover{
+                    color: $brand;
+                }
+            }
+        }
+   }
+
+</style>
